565391bf1244e4deff7b3de44cceeeaafca07f4c
Roman Miller <ra.main.w@gmail.com>
2020-02-25 16:03:17 +0300

    half done

---
+++ app/src/main/java/com/example/android/justjavaagain/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.justjavaagain;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    int numberOfCoffees = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void display(int i){
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + i);
+
+    }
+
+    private void displayPrice(int i){
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText("$" + i);
+    }
+
+    public void increment(View view) {
+        numberOfCoffees++;
+        display(numberOfCoffees);
+
+    }
+
+    public void decrement(View view) {
+        if(numberOfCoffees > 0){
+            numberOfCoffees--;
+            display(numberOfCoffees);
+        }
+    }
+
+    public void orderSubmit(View view) {
+        displayPrice(numberOfCoffees * 5);
+    }
+
+
+}
